69b3674047eef90e9e4c4be485ddc3327e6d7395
Johnny Ross <johnnytross21@gmail.com>
2019-07-09 18:23:00 -0400

    added conection file

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+//MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "192.168.99.100",
+  port: 3306,
+  user: "root",
+  password: "docker",
+  database: "burgers_db"
+});
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- create a `seeds.sql` file. In this file, 
+-- write insert queries to populate the `burgers` table
+--  with at least three entries.
+
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Baconator');
